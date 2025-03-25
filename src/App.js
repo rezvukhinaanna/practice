7e35848b79f6ce086cd978f83df2edb2c611f97d
rezvukhinaanna <113692964+rezvukhinaanna@users.noreply.github.com>
2025-03-25 10:18:51 +0300

    Настроить проект

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Div = styled.div`
+  text-align: center;
+  text-color: black;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div>
+      <i className="fa fa-calendar"></i>
+      <div>123</div>
+      <div>hello</div>
+    </Div>
   );
 }
 
