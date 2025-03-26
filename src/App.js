640b399cd2f57d7a0ea403cdc9a008573eb682b2
rezvukhinaanna <113692964+rezvukhinaanna@users.noreply.github.com>
2025-03-26 19:33:51 +0300

    Реализовать основной компонент блога с роутингом

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
+import { Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 
-const Div = styled.div`
-  text-align: center;
-  text-color: black;
+const Content = styled.div`
+  padding: 120px 0;
 `;
+
+const H2 = styled.h2`
+  text-align: center;
+`;
+
+const Header = () => <div>Шапка</div>;
+const Footer = () => <div>Футер</div>;
 
 function App() {
   return (
-    <Div>
-      <i className="fa fa-calendar"></i>
-      <div>123</div>
-      <div>hello</div>
-    </Div>
+    <>
+      <Header />
+      <Content>
+        <H2>Контент страницы</H2>
+        <Routes>
+          <Route path="/" element={<div>Главная страница</div>} />
+          <Route path="/login" element={<div>Авторизация</div>} />
+          <Route path="/register" element={<div>Регистрация</div>} />
+          <Route path="/users" element={<div>Пользователи</div>} />
+          <Route path="/post" element={<div>Новая статья</div>} />
+          <Route path="/post/:postId" element={<div>Статья</div>} />
+          <Route path="*" element={<div>Ошибка</div>} />
+        </Routes>
+      </Content>
+      <Footer />
+    </>
   );
 }
 
