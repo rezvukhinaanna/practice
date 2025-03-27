c6b048a5fba39d727eb16a4cdb5e78102946a142
rezvukhinaanna <113692964+rezvukhinaanna@users.noreply.github.com>
2025-03-27 16:57:52 +0300

    Реализовать футер

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { Header } from "./components";
+import { Header, Footer } from "./components";
 import styled from "styled-components";
 
 const AppColumn = styled.div`
@@ -19,8 +19,6 @@ const Content = styled.div`
 const H2 = styled.h2`
   text-align: center;
 `;
-
-const Footer = () => <div>Футер</div>;
 
 function App() {
   return (
