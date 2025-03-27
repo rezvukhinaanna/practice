9d55652bbfde185b513f54b9bb840c6145a269b7
rezvukhinaanna <113692964+rezvukhinaanna@users.noreply.github.com>
2025-03-27 12:01:48 +0300

    Реализовать шапку

---
+++ src/components/header/header.js
@@ -0,0 +1,32 @@
+import { ControlPanel, Logo } from "./components";
+import styled from "styled-components";
+
+const Description = styled.div`
+  font-style: italic;
+`;
+
+const HeaderContainer = ({ className }) => (
+  <header className={className}>
+    <Logo />
+    <Description>
+      Веб-технологии
+      <br />
+      Написание кода
+      <br />
+      Разбор ошибок
+    </Description>
+    <ControlPanel />
+  </header>
+);
+
+export const Header = styled(HeaderContainer)`
+  display: flex;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  width: 1000px;
+  height: 120px;
+  padding: 20px 40px;
+  box-shadow: 0px -2px 17px #000;
+  background-color: #fff;
+`;
